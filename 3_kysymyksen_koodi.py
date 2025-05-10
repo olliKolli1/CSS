ae0b450911ce0dff7b17094445f9f2caa39e0df7
Olli Korhonen <olli.a.korhonen@aalto.fi>
2025-05-10 15:46:23 +0300

    Vikoja fixauksia

--- 3_kysymyksen_koodi.py
+++ 3_kysymyksen_koodi.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from IPython.display import display
 import string
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 ehdokkaat = pd.read_csv('Ehdokas_data.csv', sep=';', encoding='utf_8')
 ehdokkaat = ehdokkaat.drop(['Tiedot', 'Vuosi', 'Puolue'], axis=1)
@@ -27,17 +29,22 @@ yhdistetyt = yleinen.merge(ehdokkaat, on='Alue', how='left')
 
 #Uusi taulukko prosenttien erotuksille
 erotus = yhdistetyt.copy()
-erotus['Miesten_prosentuaalinen_muutos'] = erotus['e_miesten_prosentti'] - erotus['y_miesten_prosentti']
-erotus['Naisten_prosentuaalinen_muutos'] = erotus['e_naisten_prosentti'] - erotus['y_naisten_prosentti']
+erotus['Change in percentages of males'] = erotus['e_miesten_prosentti'] - erotus['y_miesten_prosentti']
+erotus['Change in percentages of females'] = erotus['e_naisten_prosentti'] - erotus['y_naisten_prosentti']
 erotus = erotus.drop(['e_miesten_prosentti', 'e_naisten_prosentti', 'y_miesten_prosentti', 'y_naisten_prosentti'], axis=1)
+erotus = erotus.rename(columns={'Alue': 'Municipality'})
+
+#korrelaatio
+korrelaatio = yleinen["y_miesten_prosentti"].corr(ehdokkaat["e_miesten_prosentti"])
+print("Correlation between the male voters and candidates:", korrelaatio)
 
 #Filtteröidään
-miehiä_enemmän = erotus[erotus['Miesten_prosentuaalinen_muutos'] > 20]
-naisia_enemmän = erotus[erotus['Naisten_prosentuaalinen_muutos'] > 0]
-alle_yhden = erotus[abs(erotus['Miesten_prosentuaalinen_muutos']) < 1]
+miehiä_enemmän = erotus[erotus['Change in percentages of males'] > 20]
+naisia_enemmän = erotus[erotus['Change in percentages of females'] > 0]
+alle_yhden = erotus[abs(erotus['Change in percentages of males']) < 1]
 
 
-print("Yleineny")
+print("Yleinen")
 display(yleinen)
 
 print("Ehdokkaat")
@@ -49,18 +56,26 @@ display(yhdistetyt)
 print("Erotus:")
 display(erotus)
 
-print("Miesten muutos yli 30:")
+print("Change in the share of males over 20 percentage units:\n")
 display(miehiä_enemmän)
 
-print("Enemmän naisehdokkaita")
+print("The bigger share of female candidates than female voters:\n")
 display(naisia_enemmän)
 
-print("Alle prosentin muutos:")
+print("Change of under a percent:\n")
 display(alle_yhden)
 
-print("Dataa erotuksesta:")
-display(erotus.agg({'Miesten_prosentuaalinen_muutos': ["min", "max", "median", "mean"], 'Naisten_prosentuaalinen_muutos': ["min", "max", "median", "mean"]}))
+print("Common figures from the changes of shares in both males and females:\n")
+display(erotus.agg({'Change in percentages of males': ["min", "max", "median", "mean"], 'Change in percentages of females': ["min", "max", "median", "mean"]}))
 
 print("Dataa prosenteista:")
 display(yhdistetyt.agg({'e_miesten_prosentti': ["min", "max", "median", "mean"], 'e_naisten_prosentti': ["min", "max", "median", "mean"], 'y_miesten_prosentti': ["min", "max", "median", "mean"], 'y_naisten_prosentti': ["min", "max", "median", "mean"]}))
 
+#plottausta
+#pelkkä_miesten_muutos=erotus.drop('Naisten_prosentuaalinen_muutos', axis=1)
+#plt.figure(figsize=(20, 16))
+#sns.displot(data=pelkkä_miesten_muutos, x="Area", y="Percentual change")
+#plt.title('Change in percentage from the overall population to the candidates')
+#plt.xlabel('City')
+#plt.ylabel('Change in percentage')
+#plt.show()
